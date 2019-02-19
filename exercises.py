b60024c38f10aeda92448ca9961b08fc7dfbb688
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-19 14:00:30 -0500

    created an emotions dict

---
+++ exercises.py
@@ -0,0 +1,13 @@
+"""
+Exercise 1
+Create an emotions dict, where the keys are the names of different human emotions and the values are the degree to which the
+emotion is being felt on a scale from 1 to 3.
+"""
+
+emotions = {
+    'anger': 2,
+    'happiness': 3,
+    'fear': 1,
+    'joy': 2,
+    'sadness': 1
+}
